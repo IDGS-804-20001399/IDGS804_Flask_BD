247e16eaf98cc8cc889f10a6ee2037aafc4a5370
Roberto Aguilera <robert712.calisthenics@gmail.com>
2023-03-08 19:35:43 -0600

    03/08/2023

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask import request
+from flask import request, url_for
 from flask import redirect
 
 import forms
@@ -24,8 +24,61 @@ def index():
                        email = create_form.email.data)
         db.session.add(alum)
         db.session.commit()
+        return redirect(url_for('ABCompleto'))
     return render_template('index.html', form = create_form)
-                    
+
+
+@app.route('/ABCompleto', methods=['GET', 'POST'])
+def ABCompleto():
+    create_form = forms.UserForm(request.form)
+    #select * from alumnos
+    alumnos = Alumnos.query.all()
+    return render_template('ABCompleto.html', form = create_form, alumnos = alumnos)
+
+
+@app.route('/modificar', methods=['GET', 'POST'])
+def modificar():
+    create_form = forms.UserForm(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        # select * from alumnos where id = id
+        alumno =db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        create_form.id.data = id
+        create_form.nombre.data = alumno.nombre
+        create_form.apaterno.data = alumno.apaterno
+        create_form.email.data = alumno.email
+    if request.method == 'POST':
+        id = create_form.id.data
+        print(create_form)
+        alumno =db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        alumno.nombre = create_form.nombre.data
+        alumno.apaterno = create_form.apaterno.data
+        alumno.email = create_form.email.data
+        db.session.add(alumno)
+        db.session.commit()
+        return redirect(url_for('ABCompleto'))
+    return render_template('modificar.html', form = create_form)
+
+
+@app.route('/eliminar', methods=['GET', 'POST'])
+def eliminar():
+    create_form = forms.UserForm(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        # select * from alumnos where id = id
+        alumno =db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        create_form.id.data = id
+        create_form.nombre.data = alumno.nombre
+        create_form.apaterno.data = alumno.apaterno
+        create_form.email.data = alumno.email
+    if request.method == 'POST':
+        id = create_form.id.data
+        alumno = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        db.session.delete(alumno)
+        db.session.commit()
+        return redirect(url_for('ABCompleto'))
+    return render_template('eliminar.html', form = create_form)
+
 
 if __name__ == '__main__':
     csrf.init_app(app)
